17605c062b483b7fa460e05f7496f53006905605
doedja <111399314+doedja@users.noreply.github.com>
2025-02-21 14:47:08 +0700

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -39,6 +39,9 @@ fi
 
 echo "Rust and Cargo installed successfully."
 
+echo "Removing any existing Protobuf (protoc) installation..."
+sudo rm -rf /usr/local/bin/protoc /usr/local/include/google /usr/local/lib/libprotobuf* /usr/local/lib/pkgconfig/protobuf* ~/.protobuf*
+
 echo "Downloading Protobuf (protoc) v3.15.8..."
 curl -OL https://github.com/google/protobuf/releases/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
 
